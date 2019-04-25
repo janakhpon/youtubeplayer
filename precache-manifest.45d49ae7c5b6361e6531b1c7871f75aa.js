5967427ff9d1266a0bf7fdd9520fec4328d2399f
janakhpon <jnovaxer@gmail.com>
2019-04-26 04:55:27 +0630

    Updates

---
+++ precache-manifest.45d49ae7c5b6361e6531b1c7871f75aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da8852aeea7e0c4cb8de",
+    "url": "/youtubeplayer/static/js/runtime~main.da8852ae.js"
+  },
+  {
+    "revision": "ca3938d0d6fa23f52269",
+    "url": "/youtubeplayer/static/js/main.ca3938d0.chunk.js"
+  },
+  {
+    "revision": "102c3bf24ee7bc42981e",
+    "url": "/youtubeplayer/static/js/1.102c3bf2.chunk.js"
+  },
+  {
+    "revision": "ca3938d0d6fa23f52269",
+    "url": "/youtubeplayer/static/css/main.226e8de3.chunk.css"
+  },
+  {
+    "revision": "57adbeea83d15ce45fe9287b9035fb30",
+    "url": "/youtubeplayer/index.html"
+  }
+];
